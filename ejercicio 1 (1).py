04f1525617556373bbcd3c7303b18af885990c1b
Alejandro Espinedo <106892240+Espinedoa@users.noreply.github.com>
2022-08-12 23:12:43 -0300

    Add files via upload

---
+++ ejercicio 1 (1).py
@@ -0,0 +1,7 @@
+﻿#1) Crear una lista en Python denominada “Dueno” que contenga los siguientes valores:
+
+  #  28957346,  Juan,  Perez, 4789689,  Belgrano 101
+Dueno = [28957346, "Juan", "Perez", 4789689, "Belgrano 101"]
+
+if Dueno[0] > 26000000:
+    print(Dueno[3])
---
+++ ejercicio 2 (1).py
@@ -0,0 +1,11 @@
+﻿#2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla
+#con excepción del DNI y el apellido. Los elementos de la lista son:
+
+      #     23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”
+Dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+for i in  Dueno2 :
+    if  i==23546987 or i=="Perez" : 
+        Dueno2 = ["Maria",4789689,"Pueyrredon  811"]
+    
+print(Dueno2)
+    
